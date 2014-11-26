b9f883aaac187817e3591ca729d6b634676f933e
zepanv <zepanv@gmail.com>
2014-11-26 07:04:27 -0500

    json
    wee

--- gwycraft_common/gwydion0917/gwycraft/blocks/BlockDyedBookcase.java
+++ gwycraft_common/gwydion0917/gwycraft/blocks/BlockDyedBookcase.java
@@ -4,37 +4,37 @@ import java.util.List;
 
 import net.minecraft.block.BlockBookshelf;
 import net.minecraft.block.material.Material;
-import net.minecraft.client.renderer.texture.IIconRegister;
+//import net.minecraft.client.renderer.texture.IIconRegister;
 import net.minecraft.creativetab.CreativeTabs;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
-import net.minecraft.util.IIcon;
+//import net.minecraft.util.IIcon;
 import net.minecraft.world.World;
 import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
 
 public class BlockDyedBookcase extends BlockBookshelf {
 
-	@SideOnly(Side.CLIENT)
-	private IIcon[] iconArray;
-	private IIcon[] iconArray1;
+//	@SideOnly(Side.CLIENT)
+//	private IIcon[] iconArray;
+//	private IIcon[] iconArray1;
 
 	public BlockDyedBookcase(Material wood) {
 		super();
 	}
 
-	@Override
-	public IIcon getIcon(int par1, int par2) {
-		int l = par2 & 15;
-		return (par1 == 1 || par1 == 0) ? this.iconArray1[l]
-				: this.iconArray[l];
+//	@Override
+//	public IIcon getIcon(int par1, int par2) {
+//		int l = par2 & 15;
+//		return (par1 == 1 || par1 == 0) ? this.iconArray1[l]
+//				: this.iconArray[l];
+//
+//	}
 
-	}
-
-	@Override
-	public int damageDropped(int metadata) {
-		return metadata;
-	}
+//	@Override
+//	public int damageDropped(int metadata) {
+//		return metadata;
+//	}
 
 	@SideOnly(Side.CLIENT)
 	@Override
@@ -45,19 +45,19 @@ public class BlockDyedBookcase extends BlockBookshelf {
 		}
 	}
 
-	@SideOnly(Side.CLIENT)
-	@Override
-	public void registerBlockIcons(IIconRegister par1IconRegister) {
-		this.iconArray = new IIcon[16];
-		this.iconArray1 = new IIcon[16];
-
-		for (int i = 0; i < this.iconArray.length; ++i) {
-			this.iconArray1[i] = par1IconRegister
-					.registerIcon("gwycraft:plank_" + i);
-			this.iconArray[i] = par1IconRegister
-					.registerIcon("gwycraft:bookcase_" + i);
-		}
-	}
+//	@SideOnly(Side.CLIENT)
+//	@Override
+//	public void registerBlockIcons(IIconRegister par1IconRegister) {
+//		this.iconArray = new IIcon[16];
+//		this.iconArray1 = new IIcon[16];
+//
+//		for (int i = 0; i < this.iconArray.length; ++i) {
+//			this.iconArray1[i] = par1IconRegister
+//					.registerIcon("gwycraft:plank_" + i);
+//			this.iconArray[i] = par1IconRegister
+//					.registerIcon("gwycraft:bookcase_" + i);
+//		}
+//	}
 	
     /**
      * Determines the amount of enchanting power this block can provide to an enchanting table.
@@ -68,10 +68,10 @@ public class BlockDyedBookcase extends BlockBookshelf {
      * @return The amount of enchanting power this block produces.
      */
 
-	@Override
-    public float getEnchantPowerBonus(World world, int x, int y, int z)
-    {
-        return 1f;
-    }
+//	@Override
+//    public float getEnchantPowerBonus(World world, int x, int y, int z)
+//    {
+//        return 1f;
+//    }
 
 }
